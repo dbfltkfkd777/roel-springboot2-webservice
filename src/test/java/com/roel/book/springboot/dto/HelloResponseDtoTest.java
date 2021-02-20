aea12f2c44200e6004eeb5f87501d77c943b7243
LeeKyungHo <dbfltkfkd777>
2021-02-20 19:13:37 +0900

    롬복 설정 및 테스트.

---
+++ src/test/java/com/roel/book/springboot/dto/HelloResponseDtoTest.java
@@ -0,0 +1,23 @@
+package com.roel.book.springboot.dto;
+
+import com.roel.book.springboot.web.dto.HelloResponseDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void 롬복_기능_테스트() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
